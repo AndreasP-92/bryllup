460a41798deec48f02b4677375aedce2096c1355
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2022-03-29 20:33:04 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -84,13 +84,13 @@ export default function Home({wishes}) {
             <div className={`collapse navbar-collapse`} id="navbarNav">
               <ul className={`navbar-nav`}>
                 <li className={`nav-item active`}>
-                  <a className={`nav-link`} href="#tidsplan">Tidsplan <span className={`sr-only`}>(current)</span></a>
+                  <a className={`nav-link`} href="#tidsplan">Tidsplan</a>
                 </li>
                 <li className={`nav-item active`}>
-                    <a className={`nav-link`} href="#oenskeliste">Ønskeliste <span className={`sr-only`}>(current)</span></a>
+                    <a className={`nav-link`} href="#oenskeliste">Ønskeliste</a>
                 </li>
                 <li className={`nav-item active`}>
-                    <a className={`nav-link`} href="#ekstraInfo">Ekstra Info <span className={`sr-only`}>(current)</span></a>
+                    <a className={`nav-link`} href="#ekstraInfo">Ekstra Info</a>
                 </li>
               </ul>
             </div>
